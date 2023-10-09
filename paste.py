2217cef64e95e87459a2ffcfc0009ba925fd58d3
yav12 <80121423+yav12@users.noreply.github.com>
2023-10-08 22:00:14 -0400

    Update paste.py

--- paste.py
+++ paste.py
@@ -1,6 +1,7 @@
 import requests
 from time import sleep as wait
 from os import path
+from tokens import pastetoken
 
 def upload(author):
     while path.isfile("log.txt") == False:
@@ -8,7 +9,7 @@ def upload(author):
     with open('log.txt') as f: s = f.read()
     title = f'{author}\'s log'
     payload = {'sections':[{'name':title, 'syntax':'autodetect','contents':s}]}
-    headers = {'X-Auth-Token': 'avW54hSqZ6sKJV6WQwaOMot70JUoEgvQdkoduj7LB'}
+    headers = {'X-Auth-Token': pastetoken()}
     post_response = requests.post(url='https://api.paste.ee/v1/pastes', json=payload, headers=headers)
     with open('pastelog.txt', "a") as f: f.write(author + ": " + post_response.text)
     return post_response.text
