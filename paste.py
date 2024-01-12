bac3f1d44d8db31eea0ed75b81b49db153c29aa9
yav12 <80121423+yav12@users.noreply.github.com>
2024-01-11 20:44:07 -0500

    Update paste.py
    fixed the token business

--- paste.py
+++ paste.py
@@ -3,13 +3,14 @@ from time import sleep as wait
 from os import path
 from tokens import pastetoken
 
+token = pastetoken
 def upload(author):
     while path.isfile("log.txt") == False:
         wait(.5)
     with open('log.txt') as f: s = f.read()
     title = f'{author}\'s log'
     payload = {'sections':[{'name':title, 'syntax':'autodetect','contents':s}]}
-    headers = {'X-Auth-Token': pastetoken()}
+    headers = {'X-Auth-Token': token}
     post_response = requests.post(url='https://api.paste.ee/v1/pastes', json=payload, headers=headers)
-    with open('pastelog.txt', "a") as f: f.write(author + ": " + post_response.text)
+    with open('pastelog.txt', "a") as f: f.write(str(author) + ": " + post_response.text + '\n')
     return post_response.text
